e7abbd704ea424617357d723155f4d0662095dda
Xilong Yang <xilong.yang@foxmail.com>
2025-09-13 17:55:56 +0900

    Color Preview

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -33,6 +33,18 @@ require("lazy").setup({
          "nvim-tree/nvim-web-devicons"
       }
     },
+    {
+      "NvChad/nvim-colorizer.lua",
+      config = function()
+        require("colorizer").setup({
+          user_default_options = {
+            names = false,      -- 识别颜色名如 red/blue
+            rgb_fn = true,
+            hsl_fn = true,
+          },
+        })
+      end
+    },
     -- Completions
     {
        "saghen/blink.cmp",
